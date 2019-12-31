d1ebc73e746e054d21b8b292a4730c1fdfbb6a63
Atsushi Yamamoto <amare0130@gmail.com>
2020-01-01 03:42:45 +0900

    aaa

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Folder;
 
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index() {
-        return "Hello";
+        $folders = Folder::all();
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
